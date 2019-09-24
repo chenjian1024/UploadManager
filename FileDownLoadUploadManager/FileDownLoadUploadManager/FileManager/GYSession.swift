a37ade662f6bc1c686db7ae4ce67c232e65a475d
chenjian <1978411921@qq.com>
2019-09-24 18:45:26 +0800

    清理代码

--- FileDownLoadUploadManager/FileDownLoadUploadManager/FileManager/GYSession.swift
+++ FileDownLoadUploadManager/FileDownLoadUploadManager/FileManager/GYSession.swift
@@ -24,24 +24,8 @@ class GYSession: NSObject {
                 requestQueue: DispatchQueue? = nil,
                 serializationQueue: DispatchQueue? = nil) {
         self.session = session
-        //        precondition(session.configuration.identifier == nil,
-        //                     "Alamofire does not support background URLSessionConfigurations.")
-        //        precondition(session.delegateQueue.underlyingQueue === rootQueue,
-        //                     "Session(session:) intializer must be passed the DispatchQueue used as the delegateQueue's underlyingQueue as rootQueue.")
-        
-        self.session = session
         self.delegate = delegate
         self.rootQueue = rootQueue
-        //        self.startRequestsImmediately = startRequestsImmediately
-        //        self.requestQueue = requestQueue ?? DispatchQueue(label: "\(rootQueue.label).requestQueue", target: rootQueue)
-        //        self.serializationQueue = serializationQueue ?? DispatchQueue(label: "\(rootQueue.label).serializationQueue", target: rootQueue)
-        //        self.interceptor = interceptor
-        //        self.serverTrustManager = serverTrustManager
-        //        self.redirectHandler = redirectHandler
-        //        self.cachedResponseHandler = cachedResponseHandler
-        //        eventMonitor = CompositeEventMonitor(monitors: defaultEventMonitors + eventMonitors)
-        //        delegate.eventMonitor = eventMonitor
-        //        delegate.stateProvider = self
     }
     
     public convenience init(delegate: GYSessionDelegate = GYSessionDelegate(),
