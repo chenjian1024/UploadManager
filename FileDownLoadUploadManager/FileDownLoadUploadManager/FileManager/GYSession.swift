1b17ac844bb8595fc5647e5c310f55dafe783b67
chenjian <1978411921@qq.com>
2019-09-25 18:41:41 +0800

    ‘添加文件’

--- FileDownLoadUploadManager/FileDownLoadUploadManager/FileManager/GYSession.swift
+++ FileDownLoadUploadManager/FileDownLoadUploadManager/FileManager/GYSession.swift
@@ -12,10 +12,14 @@ class GYSession: NSObject {
     public static let `default` = GYSession()
     
     public let session: URLSession
-    
     public let delegate: GYSessionDelegate
-    
     public let rootQueue: DispatchQueue
+    public let requestQueue: DispatchQueue
+    public let serializationQueue: DispatchQueue
+    
+    var requestTaskMap = RequestTaskMap()
+    /// Set of currently active `Request`s.
+    var activeRequests: Set<Request> = []
     
     public init(session: URLSession,
                 delegate: GYSessionDelegate,
@@ -26,18 +30,27 @@ class GYSession: NSObject {
         self.session = session
         self.delegate = delegate
         self.rootQueue = rootQueue
+        self.requestQueue = requestQueue ?? DispatchQueue(label: "\(rootQueue.label).requestQueue", target: rootQueue)
+        self.serializationQueue = serializationQueue ?? DispatchQueue(label: "\(rootQueue.label).serializationQueue", target: rootQueue)
     }
     
     public convenience init(delegate: GYSessionDelegate = GYSessionDelegate(),
-                            rootQueue: DispatchQueue = DispatchQueue(label: "org.alamofire.session.rootQueue")) {
+                            rootQueue: DispatchQueue = DispatchQueue(label: "org.gy.session.rootQueue"),
+                            requestQueue: DispatchQueue? = nil,
+                            serializationQueue: DispatchQueue? = nil) {
         let configuration = URLSessionConfiguration.default
-        //        configuration.headers = .default
-        let delegateQueue = OperationQueue(maxConcurrentOperationCount: 1, underlyingQueue: rootQueue, name: "org.alamofire.session.sessionDelegateQueue")
+        let delegateQueue = OperationQueue()
+        delegateQueue.maxConcurrentOperationCount = 1
+        delegateQueue.underlyingQueue = rootQueue
+        delegateQueue.name = "org.gy.session.sessionDelegateQueue"
         let session = URLSession(configuration: configuration, delegate: delegate, delegateQueue: delegateQueue)
         
         self.init(session: session,
                   delegate: delegate,
                   rootQueue: rootQueue,
-                  delegateQueue: delegateQueue)
+                  requestQueue: requestQueue,
+                  serializationQueue: serializationQueue)
     }
+    
+    
 }
---
+++ FileDownLoadUploadManager/FileDownLoadUploadManager/FileManager/Request.swift
@@ -0,0 +1,13 @@
+//
+//  Request.swift
+//  FileDownLoadUploadManager
+//
+//  Created by chenjian on 2019/9/25.
+//  Copyright © 2019 chenjian. All rights reserved.
+//
+
+import UIKit
+
+class Request: NSObject {
+
+}
